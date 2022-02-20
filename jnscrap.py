f0aa51e75b9c387f6a6c27a62930bab7aa31b4f8
Daniel <42381829+derhnyel@users.noreply.github.com>
2022-02-20 05:59:34 +0100

    Update jnscrap.py

--- jnscrap.py
+++ jnscrap.py
@@ -4,18 +4,59 @@
 import requests
 from bs4 import BeautifulSoup as bs4 
 from fake_useragent import UserAgent
+import time
+
+
+
 UAgent = UserAgent()
-uri = https://justnaija.com/music/album/page/1
+mode = None
+
+
+
+uri = "https://justnaija.com/music/album/page/1"
 webpage = requests.get(uri,headers={"User-Agent": UAgent.random})
+
+
 ssp = bs4(webpage.content, "html.parser")
 jt = ssp.select("main article aside a")
 jp = ssp.select("main article h3 a")
 jimg = ssp.select("main article img")
+
+
+
+def func(link):
+    time.sleep(0.1)
+    resp= requests.get(link,headers={"User-Agent": UAgent.random})
+    sss = bs4(resp.text,"html.parser")
+    pg = sss.select('div[class="mu-o-c"] div[class="mu-o-unit-c"] div[class="album-side-1"]')
+    #download_link for both tracks and albums
+    download_link = sss.select('p[class="song-download"] a')[0]['href']
+    print("Album Download Link : " ,download_link)
+    cnt=1
+    for i in pg:
+        song_link = i.h4.a['href']
+        song_title = i.h4.a.text + i.span.text if i.span!=None else i.h4.a.text
+        print("---TRACK "+str(cnt)+"--- "+song_title)
+        cnt+=1
+
+    
+
+        # if i.span!=None:
+        #     #composer = i.strong.text + i.span.text
+        #     song_title = i.h4.a.text + i.span.text
+        # else:
+        #     #composer = i.strong.text '
+        #     song_title = i.h4.a.text     
+
 for index in range(len(jp)):
         title=jp[index].text
         link=jp[index]['href']
         artist=jt[index].text
         art_link=jimg[index]['data-src']
+        print("Artist Name: ",artist)
+        print('Album Title : ',title)
+        print("Album Art :" ,art_link)
+        func(link)
 
 ####Tracks
         #artist,title=jp[index].text.split("–")
@@ -26,17 +67,3 @@ for index in range(len(jp)):
         #     title = tit_ft    
        
 
-
-pg = sss.select('div[class="mu-o-c"] div[class="mu-o-unit-c"] div[class="album-side-1"]')
-#download_link for both tracks and albums
-download_link = sss.select('p[class="song-download"] a')[0]['href']
-for i in pg:
-    song_link = i.h4.a['href']
-    song_title = i.h4.a.text + i.span.text if i.span!=None else i.h4.a.text  
-    if i.span!=None:
-        #composer = i.strong.text + i.span.text
-        song_title = i.h4.a.text + i.span.text
-
-    else:
-        #composer = i.strong.text '
-        song_title = i.h4.a.text     
