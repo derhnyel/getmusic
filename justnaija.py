7755ac913c7e5a805e7c94bf893f7ac542686a72
Daniel <42381829+derhnyel@users.noreply.github.com>
2022-02-26 21:56:28 +0100

    Create justnaija.py

---
+++ justnaija.py
@@ -0,0 +1,39 @@
+from root import RootFetch
+
+
+class Fetch(RootFetch):
+    engine_name = 'Justnaija'
+    page_path='page'
+    album_path = 'album'
+    album_category='music'
+    tracks_page_path = 'music-mp3'
+
+    def __init__(self):
+        super().__init__()
+        self.site_uri = 'https://justnaija.com/'
+
+    def get_url_path(self,page=None,category=None):
+        #if page
+        return (self.album_category,self.album_path,self.page_path,page) if category=="albums" else (self.tracks_page_path,self.page_path,page)    
+    
+
+    def parse_parent_soup(soup):
+        return list(elem['href'] for elem in soup.select("main article h3 a"))
+
+
+    def parse_child_soup(soup,category=None):
+        header_elem = soup.select('div[class="mpostheader"] span[class="h1"]')[0]
+        try:
+            download_link = soup.select('p[class="song-download"] a')[0]['href']
+        except:
+            download_link = None        
+        art_link= soup.select('figure[class="song-thumbnail"] img')[0]['src']
+        if category == "albums":    
+            artist,title = header_elem.text.split(" | ")[0].split(" – ")
+            tracklist_elem = soup.select('div[class="mu-o-c"] div[class="mu-o-unit-c"] div[class="album-side-1"]')
+            for track_elem in tracklist_elem:
+                song_link = track_elem.h4.a['href']
+                song_title = track_elem.h4.a.text + track_elem.span.text if track_elem.span!=None else track_elem.h4.a.text 
+            return
+        artist,title = header_elem.text.split("] ")[1].split(" – ") 
+        return          
