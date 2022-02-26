64f4d31f5547c64ff4430777c93ce5a84cb70876
Daniel <42381829+derhnyel@users.noreply.github.com>
2022-02-26 09:19:21 +0100

    Update songslover.py

--- songslover.py
+++ songslover.py
@@ -11,28 +11,22 @@ class Fetch(RootFetch):
         super().__init__()
         self.site_uri  = "https://songslover.vip/"
 
-    def get_url_path(self,page=None,type_=None):       
-        return (type_,self.page_path,page) if type_=="albums" else (self.tracks_category,type_,self.page_path,page)
+    def get_url_path(self,page=None,format=None):       
+        return (format,self.page_path,page) if format=="albums" else (self.tracks_category,format,self.page_path,page)
 
     def parse_parent_soap(soup):
-        result = [] #use enum/dict object generator
-        soup_elements = soup.select('article h2')
-        for element in soup_elements:
-            temp_result = {}
-            try:
-                artist,title=element.text.split(' –')
-            except: 
-                title = artist = element.text
-            artist = artist.strip()
-            title = title.strip()     
-            link = element.a['href']
-            temp_result['artist']=artist 
-            temp_result['title']=title
-            temp_result['link']= link
-            result.append(temp_result)
-        return result
-    def parse_child_soup(soup):
-        dom = etree.HTML(str(soup))        
+        return list(elem['href'] for elem in soup.select('article h2 a'))
+    
+    
+    
+    def parse_child_soup(soup,format=None):
+        dom = etree.HTML(str(soup))
+        try:
+            artist,title = soup.select('div[class="post-inner"] h1 span[itemprop="name"]').text.split(' –')
+            artist,title = artist.strip(),title.strip() 
+        except:
+             artist=title=soup.select('div[class="post-inner"] h1 span[itemprop="name"]').text        
+        art_link = soup.select('div[class="entry"] img[src]')[0]['src']        
 
     
     
