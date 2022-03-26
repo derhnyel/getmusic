626f88053811ee1d59e44ae9ece47f0700987882
derhnyel <ejedenials@gmail.com>
2022-03-26 10:18:51 +0100

    Engine enhancement => mp3juices search function implemented to return values

--- engine/mp3juices.py
+++ engine/mp3juices.py
@@ -1,8 +1,10 @@
 
 from engine.root import BaseEngine
+import json
 class Mp3Juices(BaseEngine):
 
-    api_path = 'api/search.php'
+    api_path = 'api'
+    search_path = 'search.php?'
 
     def __init__(self):
         super().__init__()
@@ -10,32 +12,16 @@ class Mp3Juices(BaseEngine):
         self.request_method = self.POST
 
     def get_query_params(self,query=None,page=None,**kwargs):
-        if kwargs.get("id"):
-            return dict(id=kwargs.pop(id))
         return  dict(q=query,page=page)
 
     def get_url_path(self, page=None, category=None):
-        return (self.api_path)
+        return (self.api_path,self.search_path)
 
-    def parse_parent_object(self,json,**kwargs):
-        #extract songs id from json
-        #extract song art
-        # track details
-        return
-
-    def parse_single_object(self,id=None,category=None,**kwargs):
-        return self.get_formated_url(
-            self,
-            url = "https://idmp3s.com/",
-            params = self.get_query_params(**{'id':id}),
-            method = self.GET,path=("api/vip/get_song.php"),
-        )
-
-    def search(self, query=None, page=None, category=None, **kwargs):
-        json = self.get_soup(url=self.get_formated_url(**kwargs),payload=self.get_query_params(query=query,page=0))
-        response = self.parse_parent_object(json)        
-        return response
-         
+    def search(self, query=None, page=0, **kwargs):
+        response = self.get_response_object(url=self.get_formated_url(**kwargs),payload=self.get_query_params(query=query,page=page))
+        response = json.loads(response.text.strip("();\n"))["response"]
+        return list(dict(artist=item["artist"],title=item['title'],category_download=item["url"], track_length=item['duration']/60) for item in response if isinstance(item,dict))
+    
 
 
 
